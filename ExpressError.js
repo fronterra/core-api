9704caa53a87f572ff27414d30ced9bdadd9caed
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-03-08 19:08:03 -0800

    add new custom error

---
+++ ExpressError.js
@@ -0,0 +1,12 @@
+/**
+ * Custom error class
+ */
+class ExpressError extends Error {
+    constructor(message, status) {
+        super();
+        this.message = message;
+        this.status = status;
+    }
+}
+
+module.exports = ExpressError;
