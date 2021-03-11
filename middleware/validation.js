4a93eb12311b47a4868a2bf3e9990c99a14b11e7
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-03-10 22:00:44 -0800

    switched order to valdiation ops in middleware

--- middleware/validation.js
+++ middleware/validation.js
@@ -5,11 +5,11 @@ const ExpressError = require('../ExpressError');
  */
 function validateMediaUpload(request, response, next) {
     try {
+        // throws error if request is missing either its reportId or mediaId
+        if (!request.body.reportId || !request.body.mediaId) throw new ExpressError('Request body must contain both of the following properties: mediaId, reportId', 412);
+
         // throws error if no file was sent with request
         if (!request.files) throw new ExpressError('No files recieved', 412) // 412 status code === 'Precondition Not Met'
-
-        // throws error if request is missing either its reportId or mediaId
-        if (!request.body.reportId || !request.body.mediaId) throw new ExpressError('Request body must contain both of the following properties: mediaId, reportId');
 
         // if no errors pass on to the function block
         return next();
