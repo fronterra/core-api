78d9d9d2b1b3fbf1e1ac408fcf0dad0a4a3c03be
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-03-10 20:46:57 -0800

    improved docs and moved validation to middleware

--- routes/media.js
+++ routes/media.js
@@ -3,15 +3,18 @@ const router = express.Router();
 const s3Bucket = require('../services/s3Bucket');
 const ExpressError = require('../ExpressError');
 const { AWS_S3_NAME: bucketName, AWS_S3_REGION: region } = require('../config');
+const { validateMediaUpload } = require('../middleware/validation');
 
-router.post('/', async function (request, response, next) {
+/**
+ * `POST /media`
+ * 
+ * - Calls express-fileupload middleware to parse the request header for content-types of `multipart/formData`
+ * - If founds, recieved files are set on the `request.files` property
+ * - Custom middleware `validateMediaUpload` is called to validate the file upload and that both `reportId` and `mediaId` were passed in `request.body`.
+ * 
+ */
+router.post('/', validateMediaUpload, async function (request, response, next) {
     try {
-        // throws error if no file was sent with request
-        if (!request.files) throw new ExpressError('No files recieved', 412) // 412 status code === 'Precondition Not Met'
-
-        // throws error if request is missing either its reportId or mediaId
-        if (!request.body.reportId || !request.body.mediaId) throw new ExpressError('Request body must contain both of the following properties: mediaId, reportId');
-
         // destructure variables from request body
         const { reportId, mediaId } = request.body;
 
@@ -26,8 +29,11 @@ router.post('/', async function (request, response, next) {
             groupType: 'report'
         };
 
+        // destructure request.files object
+        const { mediaFile } = request.files;
+
         // send file to s3 bucket
-        const res = await bucketOperations.uploadObject(keysObject, request.files.mediaFile.data);
+        const res = await bucketOperations.uploadObject(keysObject, mediaFile.data);
 
         return response.status(201).json({ res });
     } catch (err) {
