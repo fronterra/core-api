1ed75cb1be16495303853c7654fc1e8513933170
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-03-13 16:36:18 -0800

    updated tests to look for reportId and mediaId in qstring params

--- __tests__/unit/media.test.js
+++ __tests__/unit/media.test.js
@@ -11,26 +11,26 @@ const request = supertest(app);
 
 
 describe('POST /media', function() {
-    it('should return error message with 412 status code when no file is uploaded with the request', async function() {
+    it('should return error message with 412 status code when either mediaId or reportId is missing', async function() {
         // creates fake media id similar to what would typically be passed
         const fakeMediaId = String(new ObjectId()); 
 
-        // mock request
-        const response = await request.post('/media').send({ reportId: undefined, mediaId: fakeMediaId });
+        // mock request without reportId or mediaFile attached should throw error about reportId first.
+        const response = await request.post(`/media?mediaId=${fakeMediaId}`);
 
         // test response message and status
-        expect(response.body.message).toStrictEqual('Request body must contain both of the following properties: mediaId, reportId');
+        expect(response.body.message).toStrictEqual('Query string parameters must contain both of the following properties: mediaId, reportId');
         expect(response.body.status).toBe(412);
     });
 
 
-    it('should return error message with 412 status code and message about file requirement', async function () {
+    it('should return error message with 412 status code when no file is uploaded with the request', async function () {
         // creates fake report and media ids similar to what would typically be passed
         const fakeMediaId = String(new ObjectId()); 
         const fakeReportId = String(new ObjectId());
 
-        // mock request
-        const response = await request.post('/media').send({ reportId: fakeReportId , mediaId: fakeMediaId });
+        // mock request without mediaFile attachment, but with reportId and mediaId, should throw error about missing file
+        const response = await request.post(`/media?reportId=${fakeReportId}&mediaId=${fakeMediaId}`);
 
         // test response message and status
         expect(response.body.message).toStrictEqual('No files recieved');
