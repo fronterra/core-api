473412518f51e4a31f8498950cef840024fac3b6
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-03-14 23:56:39 -0700

    added POST /reports route

---
+++ routes/reports.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+// router
+const router = express.Router();
+
+router.post('/', async function(request, response, next) {
+    try {
+        // get values from request body
+        const { description, location, locationType, datetime, datetimeType, pollutionType, mediaIds, mediaUploadSuccesses, mediaUploadFailures } = request.body;
+
+        // send information
+
+        // return 201
+        return response.status(201);
+
+    } catch(err) {
+        return next(err);
+    }
+});
+
