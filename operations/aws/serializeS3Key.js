3b91eab7f8f6339e11a277921d4d83c41456e454
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-04-04 16:34:15 -0700

    moved all aws files into new `operations/aws` dir

---
+++ operations/aws/serializeS3Key.js
@@ -0,0 +1,38 @@
+const ExpressError = require('../../ExpressError');
+
+const checkInputs = (type, inputs) => {
+    for (let input of inputs) {
+        if (typeof input !== type) return false;
+    }
+    return true;
+}
+
+/**
+ * Takes four strings as arguments and returns a concatination
+ * of all into one path
+ * 
+ * @param {String} itemType 
+ * @param {String} itemId 
+ * @param {String} groupType 
+ * @param {String} groupId 
+ * 
+ * @returns {String}
+ * 
+ * @example
+ * ```js
+ * > const serialized = serializeS3Key('report', '1234', 'media', '4321')
+ * > console.log(serialized);
+ * 'report/1234/media/4321'
+ * ```
+ */
+const serializeS3Key = (itemType, itemId, groupType, groupId) => {
+    // throws error if inputs are of incorrect type
+    if (!checkInputs('string', [itemType, itemId, groupType, groupId])) throw new ExpressError('All arguments must be strings', 400);
+
+    // return serialized key
+    return `${groupType}/${groupId}/${itemType}/${itemId}`;
+}
+
+
+
+module.exports = serializeS3Key;
