534877a7b9394a49c51020f3347837286a4db158
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-04-04 16:31:35 -0700

    moved `middleware` into `controllers`

---
+++ controllers/middleware/validation.js
@@ -0,0 +1,28 @@
+const ExpressError = require('../../ExpressError');
+
+/**
+ * Validates incoming request body for following
+ * properties:
+ * - `mediaFile` *from* `request.body.files`
+ * 
+ * 
+ * And query string params for the following:
+ * - `mediaId`
+ * - `reportId`
+ */
+function validateMediaUpload(request, response, next) {
+    try {
+        // throws error if request is missing either its reportId or mediaId
+        if (!request.query.reportId || !request.query.mediaId) throw new ExpressError('Query string parameters must contain both of the following properties: mediaId, reportId', 412);
+
+        // throws error if no file was sent with request
+        if (!request.files) throw new ExpressError('No files recieved', 412) // 412 status code === 'Precondition Not Met'
+
+        // if no errors pass on to the function block
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+}
+
+module.exports = { validateMediaUpload };
