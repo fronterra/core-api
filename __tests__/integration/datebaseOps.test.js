c0580554e27bc4032708ff793dcdbf0b3b881ec4
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-04-03 22:56:22 -0700

    deleted integration test file

--- __tests__/integration/datebaseOps.test.js
+++
@@ -1,9 +0,0 @@
-const databaseOps = require('../../services/databaseOps');
-
-// describe('tests write operation functions returned by databaseOps', function() {});
-
-// describe('tests read operations function returned by databaseOps', function() {});
-
-// describe('tests update operations functions returned by databaseOps', function() {});
-
-// describe('tests delete operations functions returned by databaseOps', function() {});
